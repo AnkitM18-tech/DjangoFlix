b70f514ff8c72001173370f4eb7ca7e54462a532
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2021-11-11 01:18:11 +0530

    added args to Charfields

--- src/videos/models.py
+++ src/videos/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Video(models.Model):
-    title = models.CharField()
-    description = models.TextField()
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True, null=True)
     slug = models.SlugField(blank=True,null=True) #This is my video
-    video_id = models.CharField()
+    video_id = models.CharField(max_length=200)
     # timestamp
     # updated
     # state
