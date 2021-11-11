1961ca7c5f5f9140b931a8758e4ed5601864eeb5
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2021-11-12 01:58:36 +0530

    Chnaged VideoProxy to VideoPublished and VideoAll

--- src/videos/models.py
+++ src/videos/models.py
@@ -6,13 +6,20 @@ class Video(models.Model):
     description = models.TextField(blank=True, null=True)
     slug = models.SlugField(blank=True,null=True) #This is my video
     video_id = models.CharField(max_length=200)
+    active = models.BooleanField(default=True)
     # timestamp
     # updated
     # state
     # publish_timestamp 
 
-class VideoProxy(Video):
+class VideoPublishedProxy(Video):
     class Meta:
         proxy=True
         verbose_name = 'Published Video'
         verbose_name_plural = 'Published Videos'
+
+class VideoAllProxy(Video):
+    class Meta:
+        proxy=True
+        verbose_name = 'All Video'
+        verbose_name_plural = 'All Videos'
