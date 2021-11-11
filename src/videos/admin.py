03f0551e38af04484630f65c82c64d9c54124705
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2021-11-12 01:57:55 +0530

    VideoPublishedProxy and VideoAllProxy registered

--- src/videos/admin.py
+++ src/videos/admin.py
@@ -1,6 +1,25 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Video,VideoProxy
-admin.site.register(Video)
-admin.site.register(VideoProxy)
+from .models import VideoAllProxy,VideoPublishedProxy
+
+class VideoAllAdmin(admin.ModelAdmin):
+    list_display = ['title','video_id']
+    search_fileds = ['title']
+    # list_filter = ['video_id']
+    class Meta:
+        model = VideoAllProxy
+
+admin.site.register(VideoAllProxy,VideoAllAdmin)
+
+class VideoPublishedProxyAdmin(admin.ModelAdmin):
+    list_display = ['title','video_id']
+    search_fileds = ['title']
+    # list_filter = ['video_id']
+    class Meta:
+        model = VideoPublishedProxy
+
+    def get_queryset(self,request):
+        return VideoAllProxy.objects.filter(active=True)
+
+admin.site.register(VideoPublishedProxy,VideoPublishedProxyAdmin)
