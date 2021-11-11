f7b775a11cb40f39272144e45a1e1159d5973cd0
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2021-11-12 01:40:19 +0530

    registered VideoProxy

--- src/videos/admin.py
+++ src/videos/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Video
+from .models import Video,VideoProxy
 admin.site.register(Video)
+admin.site.register(VideoProxy)
