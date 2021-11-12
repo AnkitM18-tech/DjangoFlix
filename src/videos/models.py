0a28f5c54fb3da683458f5612c7cba482d1f9a2a
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2021-11-13 03:15:53 +0530

    added slugs, timestamp and updated field

--- src/videos/models.py
+++ src/videos/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.utils.text import slugify
 # Create your models here.
 class Video(models.Model):
     class VideoStateOptions(models.TextChoices):
@@ -13,12 +14,13 @@ class Video(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True, null=True)
     slug = models.SlugField(blank=True,null=True) #This is my video
-    video_id = models.CharField(max_length=200)
+    video_id = models.CharField(max_length=200, unique=True)
     active = models.BooleanField(default=True)
-    # timestamp
-    # updated
+    timestamp = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
     state = models.CharField(max_length=2, choices=VideoStateOptions.choices,default=VideoStateOptions.DRAFT)
     publish_timestamp = models.DateTimeField(auto_now_add=False,auto_now=False,blank=True,null=True)
+
     @property
     def is_published(self):
         return self.active
@@ -29,6 +31,8 @@ class Video(models.Model):
             self.publish_timestamp = timezone.now()
         elif self.state == self.VideoStateOptions.DRAFT:
             self.publish_timestamp = None
+        if self.slug is None:
+            self.slug = slugify(self.title)
         super().save(*args,**kwargs)
 
 class VideoPublishedProxy(Video):
