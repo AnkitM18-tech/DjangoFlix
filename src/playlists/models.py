16486b93725c6fb0700398bde0e35472446facca
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2021-11-13 22:57:44 +0530

    Created PlayList model

---
+++ src/playlists/models.py
@@ -0,0 +1,52 @@
+from django.db import models
+from django.utils import timezone
+# from django.utils.text import slugify
+from django.db.models.signals import pre_save
+# Create your models here.
+from videos.models import Video
+from djangoflix.db.models import PublishStateOptions
+from djangoflix.db.receivers import publish_state_pre_save,slugify_pre_save
+
+class PlayListQuerySet(models.QuerySet):
+    def published(self):
+        now= timezone.now()
+        return self.filter(publish_timestamp__lte = now,state=PublishStateOptions.PUBLISH)
+
+class PlayListManager(models.Manager):
+    def get_queryset(self):
+        return PlayListQuerySet(self.model, using=self._db)
+
+    # def published(self):
+    #     return self.get_queryset().published()
+
+class PublishStateOptions(models.TextChoices):
+    #CONSTANT = DB_VALUE, USER_DISPLAY_VALUE
+    PUBLISH = "PU","Publish"
+    DRAFT = "DR","Draft"
+    UNLISTED = "UN","Unlisted"
+    PRIVATE = "PR","Private"
+
+class PlayList(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True, null=True)
+    slug = models.SlugField(blank=True,null=True)
+    video = models.ForeignKey(Video,null=True,on_delete=models.SET_NULL)   #One Video Per Playlist
+    active = models.BooleanField(default=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    state = models.CharField(max_length=2, choices=PublishStateOptions.choices,default=PublishStateOptions.DRAFT)
+    publish_timestamp = models.DateTimeField(auto_now_add=False,auto_now=False,blank=True,null=True)
+
+    objects = PlayListManager()
+
+    @property
+    def is_published(self):
+        return self.active
+
+    # def save(self,*args,**kwargs):
+    #     if self.slug is None:
+    #         self.slug = slugify(self.title)
+    #     super().save(*args,**kwargs)
+
+pre_save.connect(publish_state_pre_save,sender=PlayList)
+pre_save.connect(slugify_pre_save,sender=PlayList)
