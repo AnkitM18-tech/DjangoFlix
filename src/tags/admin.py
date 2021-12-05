dc141070c2d8193994da3872895b2bb5c2e04659
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2021-12-06 01:43:30 +0530

    tags app init files

---
+++ src/tags/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import TaggedItem
+# Register your models here.
+
+class TaggedItemAdmin(admin.ModelAdmin):
+    fields = ['tag','content_type','object_id','content_object']
+    readonly_fields = ['content_object']
+    class Meta:
+        model = TaggedItem
+
+admin.site.register(TaggedItem,TaggedItemAdmin)
---
+++ src/tags/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
+# Create your models here.
+class TaggedItem(models.Model):
+    tag = models.SlugField()
+    content_type = models.ForeignKey(ContentType,on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey("content_type","object_id")
+
+    # def get_related_object(self):
+    #     Klass  = self.content_type.model_class()
+    #     return Klass.objects.get(id=self.object_id)
