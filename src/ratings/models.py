d94d8cfb54185b4e74535fce988984f279843e9d
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2021-12-06 20:25:55 +0530

    Rating Model

--- src/ratings/models.py
+++ src/ratings/models.py
@@ -1,3 +1,22 @@
 from django.db import models
-
+from django.conf import settings
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 # Create your models here.
+
+User = settings.AUTH_USER_MODEL
+
+class RatingChoices(models.IntegerChoices):
+    ONE = 1
+    TWO = 2
+    THREE = 3
+    FOUR = 4
+    FIVE = 5
+    __empty__ = "UNKNOWN"
+
+class Rating(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    value = models.IntegerField(null=True,blank=True,choices=RatingChoices.choices)
+    content_type = models.ForeignKey(ContentType,on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey("content_type","object_id")
