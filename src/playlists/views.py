108c90f2ca0f2e89bbf17e3ab581d7ffae539cec
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2021-12-09 22:09:30 +0530

    added list view for movies, shows and featured

--- src/playlists/views.py
+++ src/playlists/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
-
+# from django.shortcuts import render
+from django.views.generic import ListView
 # Create your views here.
+from .models import MovieProxy,TVShowProxy,PlayList
+
+class PlayListMixin():
+    title = None
+    template_name = "playlist_list.html"
+
+    def get_context_data(self,*args,**kwargs):
+        context = super().get_context_data(*args,**kwargs)
+        if self.title is not None:
+            context["title"] = self.title
+        print(context)
+        return context
+
+class MovieListView(PlayListMixin,ListView):
+    queryset = MovieProxy.objects.all()
+    title = "Movies"
+
+class TVShowListView(PlayListMixin,ListView):
+    queryset = TVShowProxy.objects.all()
+    title = "TV Shows"
+
+class FeaturedPlayListListView(PlayListMixin,ListView):
+    template_name = "featured_playlist_list.html"
+    # queryset = PlayList.objects.featured_playlists()
+    title = "Featured"
