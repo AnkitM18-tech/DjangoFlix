b5898ae5a8fa09e03160655f8b20a261f73a0ca1
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2021-12-11 03:09:31 +0530

    added list views and details views

--- src/djangoflix/urls.py
+++ src/djangoflix/urls.py
@@ -13,13 +13,27 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+'''
+str -> anything but /
+int -> 0 and up
+slug -> this-is-slug
+uuid -> import uuid ---> uuid.uuid4()
+path -> abc/bac/cba
+'''
+
 from django.contrib import admin
 from django.urls import path
-from playlists.views import MovieListView,TVShowListView,FeaturedPlayListListView
+from playlists.views import MovieDetailView, MovieListView,TVShowListView,FeaturedPlayListListView,MovieDetailView,PlayListDetailView,TVShowDetailView,TVShowSeasonDetailView
 
 urlpatterns = [
     path('', FeaturedPlayListListView.as_view()),
     path('admin/', admin.site.urls),
     path('movies/', MovieListView.as_view()),
+    path('movies/<slug:slug>/', MovieDetailView.as_view()),
+    path('media/<int:pk>/',PlayListDetailView.as_view()),
+    path('shows/<slug:showSlug>/seasons/<slug:seasonSlug>/', TVShowSeasonDetailView.as_view()),
+    path('shows/<slug:slug>/seasons/', TVShowDetailView.as_view()),
+    path('shows/<slug:slug>/', TVShowDetailView.as_view()),
     path('shows/', TVShowListView.as_view()),
 ]
