8d4ae306350ed0e948642816bbf5e7014793625b
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2021-12-11 03:09:55 +0530

    added List and details view classes

--- src/playlists/views.py
+++ src/playlists/views.py
@@ -1,7 +1,8 @@
 # from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView,DetailView
+from django.http import Http404
 # Create your views here.
-from .models import MovieProxy,TVShowProxy,PlayList
+from .models import MovieProxy,TVShowProxy,PlayList,TVShowSeasonProxy
 
 class PlayListMixin():
     title = None
@@ -21,9 +22,34 @@ class MovieListView(PlayListMixin,ListView):
     queryset = MovieProxy.objects.all()
     title = "Movies"
 
+class MovieDetailView(PlayListMixin,DetailView):
+    template_name = "playlists/movie_detail.html"
+    queryset = MovieProxy.objects.all()
+
+class PlayListDetailView(PlayListMixin,DetailView):
+    template_name = "playlists/playlist_detail.html"
+    queryset = PlayList.objects.all()
+
 class TVShowListView(PlayListMixin,ListView):
     queryset = TVShowProxy.objects.all()
     title = "TV Shows"
+
+class TVShowDetailView(PlayListMixin,DetailView):
+    template_name = "playlists/tvshow_detail.html"
+    queryset = TVShowProxy.objects.all()
+
+class TVShowSeasonDetailView(PlayListMixin,DetailView):
+    template_name = "playlists/season_detail.html"
+    queryset = TVShowSeasonProxy.objects.all()
+
+    def get_object(self):
+        kwargs = self.kwargs
+        show_slug = kwargs.get("showSlug")
+        season_slug = kwargs.get("seasonSlug")
+        qs  =self.get_queryset().filter(parent__slug__iexact=show_slug,slug__iexact=season_slug)
+        if not qs.count() == 1:
+            raise Http404
+        return qs.first()
 
 class FeaturedPlayListListView(PlayListMixin,ListView):
     # template_name = "featured_playlist_list.html"
