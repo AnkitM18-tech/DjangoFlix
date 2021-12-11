9fc03dcc02083a85b91dd8978ebcc5957d8fe45d
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2021-12-12 03:15:18 +0530

    modified PRoxy settings

--- src/videos/admin.py
+++ src/videos/admin.py
@@ -24,6 +24,6 @@ class VideoPublishedProxyAdmin(admin.ModelAdmin):
         model = VideoPublishedProxy
 
     def get_queryset(self,request):
-        return VideoAllProxy.objects.filter(active=True)
+        return VideoPublishedProxy.objects.filter(active=True)
 
 admin.site.register(VideoPublishedProxy,VideoPublishedProxyAdmin)
