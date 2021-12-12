c29f41c6b3ea192097f6024f9322aac5cb2e55e8
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2021-12-12 20:51:06 +0530

    views for category

--- src/categories/views.py
+++ src/categories/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
-
+from django.http import Http404
+from django.views.generic import ListView,DetailView
+from django.db.models import Count
+from .models import Category
+from playlists.models import PlayList
+from playlists.mixins import PlayListMixin
 # Create your views here.
+
+class CategoryListView(ListView):
+    queryset = Category.objects.all().filter(active=True).annotate(pl_count=Count('playlists')).filter(pl_count__gt=0)
+
+
+class CategoryDetailView(PlayListMixin,ListView):
+    """ another ListView for PlayLists """
+    def get_context_data(self):
+        context = super().get_context_data()
+        try:
+            obj = Category.objects.get(slug=self.kwargs.get('slug'))
+        except Category.DoesNotExist:
+            raise Http404
+        except Category.MultipleObjectsReturned:
+            raise Http404
+        except:
+            obj = None
+        context['object'] = obj
+        if obj is not None:
+            context['title'] = obj.title
+        return context
+
+    def get_queryset(self):
+        slug = self.kwargs.get('slug')
+        return PlayList.objects.filter(category__slug=slug).movie_or_show()
