da2f7d248f6812e81a616a584b3b217759c8f731
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2021-12-14 03:25:35 +0530

    rate_object_view method

--- src/ratings/views.py
+++ src/ratings/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from .forms import RatingForm
+from .models import Rating
+from django.contrib.contenttypes.models import ContentType
 
-# Create your views here.
+def rate_object_view(request):
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect('/')
+    if request.method == 'POST':
+        form = RatingForm(request.POST)
+        if form.is_valid():
+            object_id = form.cleaned_data.get('object_id')
+            rating = form.cleaned_data.get('rating')
+            content_type_id = form.cleaned_data.get('content_type_id')
+            c_type = ContentType.objects.get_for_id(content_type_id)
+            obj= Rating.objects.create(
+                content_type=c_type,
+                object_id = object_id,
+                value=rating,
+                user=request.user,
+            )
+            next = form.cleaned_data.get('next')
+            return HttpResponseRedirect(next)
+    return HttpResponseRedirect('/')
